8e9114760e33147f9202c22000c1c60c00872b09
Harrison <grodinh@winchesterthurston.org>
2016-12-05 13:13:44 -0500

    Add server integration to the client

--- client.py
+++ client.py
@@ -1,4 +1,9 @@
-from swish.recognize import get_result_response, listen, speech_to_text
+import json
+
+import requests
+
+import config
+from swish.recognize import listen, speech_to_text
 from swish.speak import speak
 
 try:
@@ -11,9 +16,25 @@ try:
 
         print("YOU SAID:", text)
 
-        for response in get_result_response(text):
-            print("SAYING:", response)
-            speak(response)
+        response = requests.get(
+            config.BASE_URL + "response",
+            headers={
+                "Content-Type": "application/json"
+            },
+            data=json.dumps({
+                "text": text
+            })
+        ).json()
+
+        print(response)
+
+        if "response" in response:
+            to_speak = response["response"]
+        else:
+            to_speak = "An error occured."
+
+        print("SAYING:", to_speak)
+        speak(to_speak)
 
         if (input("Continue? [Y/n] ").upper() or 'Y') != 'Y':
             break
