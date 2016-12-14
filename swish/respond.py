4636deb6402c842685bfd820990de54b631ec602
Harrison <grodinh@winchesterthurston.org>
2016-12-14 14:44:53 -0500

    Add plastic number response

--- swish/respond.py
+++ swish/respond.py
@@ -11,7 +11,12 @@ def respond_plastic(intent, rdb_conn, *, keyword="PlasticKeyword"):
 
     return r.table("responses").filter({
         "is_recyclable": item["number"] in RECYCLABLE_PLASTICS
-    }).sample(1).run(rdb_conn)[0]["response"].format(item=item["name"])
+    }).sample(1).run(rdb_conn)[0]["response"].format(
+        item=item["name"]
+    ) + " Number {n} plastic is {r}recyclable.".format(
+        n=item["number"],
+        r="" if item["number"] in RECYCLABLE_PLASTICS else "not "
+    )
 
 
 def respond(intent, rdb_conn, *, keyword):
