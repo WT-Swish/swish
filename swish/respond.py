4827bf8face35aea3497d55d70ed0266e99ed59b
Harrison <grodinh@winchesterthurston.org>
2016-12-14 15:03:22 -0500

    Improve response function

--- swish/respond.py
+++ swish/respond.py
@@ -1,3 +1,5 @@
+import random
+
 import rethinkdb as r
 
 RECYCLABLE_PLASTICS = (1, 2, 3, 4, 5, 7)
@@ -5,15 +7,9 @@ RECYCLABLE_PLASTICS = (1, 2, 3, 4, 5, 7)
 
 def respond_plastic(intent, rdb_conn, *, keyword="PlasticKeyword"):
 
-    item = next(r.table("items").filter({
-        "name": intent[keyword]
-    }).run(rdb_conn))
+    item, response = respond(intent, rdb_conn, keyword=keyword)
 
-    return r.table("responses").filter({
-        "is_recyclable": item["number"] in RECYCLABLE_PLASTICS
-    }).sample(1).run(rdb_conn)[0]["response"].format(
-        item=item["name"]
-    ) + " Number {n} plastic is {r}recyclable.".format(
+    return response + " Number {n} plastic is {r}recyclable.".format(
         n=item["number"],
         r="" if item["number"] in RECYCLABLE_PLASTICS else "not "
     )
@@ -25,6 +21,9 @@ def respond(intent, rdb_conn, *, keyword):
         "name": intent[keyword]
     }).run(rdb_conn))
 
-    return r.table("responses").filter({
+    if "responses" in item:
+        return item, random.choice(item["responses"])
+
+    return item, r.table("responses").filter({
         "is_recyclable": item["recyclable"]
     }).sample(1).run(rdb_conn)[0]["response"].format(item=item["name"])
